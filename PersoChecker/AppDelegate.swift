f9b51ed3ad4f8018da4f0f21c4a4b7ece3bd52ad
Sebastian Kipping <kipping.sebastian@gmail.com>
2016-04-01 21:11:35 +0200

    Initial Commit

---
+++ PersoChecker/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  PersoChecker
+//
+//  Created by Sebastian Kipping on 01.04.16.
+//  Copyright © 2016 Sebastian Kipping. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ PersoChecker/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  PersoChecker
+//
+//  Created by Sebastian Kipping on 01.04.16.
+//  Copyright © 2016 Sebastian Kipping. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
