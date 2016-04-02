bf2754dd42b661a78e1657aebac30cc28f6a5225
Sebastian Kipping <kipping.sebastian@gmail.com>
2016-04-02 13:15:31 +0200

    Add blocks model

---
+++ PersoChecker/Blocks.swift
@@ -0,0 +1,16 @@
+//
+//  Blocks.swift
+//  PersoChecker
+//
+//  Created by Sebastian Kipping on 02.04.16.
+//  Copyright © 2016 Sebastian Kipping. All rights reserved.
+//
+
+import Foundation
+
+enum Blocks {
+    case Block1
+    case Block2
+    case Block3
+    case Block4
+}
